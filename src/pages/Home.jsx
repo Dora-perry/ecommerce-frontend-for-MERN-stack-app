daa77adfe55519a76a10a56cc27cde4d9fcb3e75
Decagon <theodorachinonso@gmail.com>
2022-12-27 13:25:20 +0100

    announcement component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Navbar from '../Components/Navbar'
+import Announcement from '../Components/Announcement'
 
 const Home = () => {
-  return (
-      <Navbar/>
-      
+    return (
+      <>
+      <Announcement/>
+      <Navbar />
+      </>
   )
 }
 
