2ab4473d9160318ea4d4ac8cff874fd0e951d1ea
Decagon <theodorachinonso@gmail.com>
2022-12-28 00:56:56 +0100

    newsletterand category section

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+  {
+    id: 1,
+    img: 'https://i.ibb.co/DG69bQ4/2.png',
+    title: 'SUMMER SALE',
+    desc: " DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS ",
+    bg: 'f5fafd',
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/DG69bQ4/2.png',
+    title: 'AUTUMN COLLECTION',
+    desc: " DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS ",
+    bg: 'fcf1ed',
+  },
+  {
+    id: 3,
+    img: 'https://i.ibb.co/DG69bQ4/2.png',
+    title: 'LOUNGEWEAR LOVE',
+    desc: " DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS ",
+    bg: 'f5fafd',
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg?auto=compresss&cs=tiny',
+    title: 'SHIRT STYLE',
+  },
+  {
+    id: 2,
+    img: 'https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tiny',
+    title: 'LIGHT JACKET',
+  },
+  {
+    id: 3,
+    img: 'https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tiny',
+    title: 'LOUNGEWEAR LOVE',
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: 'https://www.vintageindustries.nl/download_front/ykadsw2018/23103_Riley_shirt_Red_check.png',
+  },
+  {
+    id: 2,
+    img: 'https://www.vintageindustries.nl/download_front/ioshaq2075/3543_Darwin_shirt_jacket_Orange_check.png',
+  },
+  {
+    id: 3,
+    img: 'https://www.vintageindustries.nl/download_front/psuizl2041/3547_Grant_T_shirt_Ocean_Blue.png',
+  },
+  {
+    id: 4,
+    img: 'https://www.vintageindustries.nl/download_front/lenyxz2154/_overview_large_/3020_Cruz_Hooded_Sweater_Mid_Grey_Front_2.png',
+  },
+  {
+    id: 5,
+    img: 'https://www.vintageindustries.nl/download_front/bwenza1748/3029_Greeley_Crewneck_sweat_Duck.png',
+  },
+  {
+    id: 6,
+    img: 'https://www.vintageindustries.nl/download_front/rxvtqb2136/_overview_large_/30120_Valerio_Fleece_Brick.png',
+  },
+  {
+    id: 7,
+    img: 'https://www.vintageindustries.nl/download_front/wlyfom1580/3538_Austin_shirt_Blue_Check.png',
+  },
+  {
+    id: 8,
+    img: 'https://www.pngarts.com/files/5/Black-Jacket-PNG-Pic.png',
+  },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,20 @@ import React from 'react'
 import Navbar from '../Components/Navbar'
 import Announcement from '../Components/Announcement'
 
+import Slider from '../Components/Slider'
+import Categories from '../Components/Categories'
+import Products from '../Components/Products'
+import Newsletter from '../Components/Newsletter'
+
 const Home = () => {
     return (
       <>
       <Announcement/>
-      <Navbar />
+            <Navbar />
+        <Slider />
+        <Categories />
+        <Products />
+        <Newsletter/>
       </>
   )
 }
